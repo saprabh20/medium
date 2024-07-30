716d6f2aafea43f630633b5abb8d04b5d4b388de
saprabh20 <saprabhanand20@gmail.com>
2024-07-31 01:36:10 +0530

    first commit

---
+++ common/src/index.ts
@@ -0,0 +1,38 @@
+import z from  "zod";
+
+export const signupInput = z.object({
+    username: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().optional(),
+});
+
+
+export const signinInput = z.object({
+    username: z.string().email(),
+    password: z.string().min(6),
+});
+
+
+export const createBlogInput = z.object({
+    title: z.string(),
+    content: z.string(),
+});
+
+
+export const updateBlogInput = z.object({
+    id: z.number(),
+    title: z.string().optional(),
+    content: z.string().optional(),
+});
+
+
+export const deleteBlogInput = z.object({
+    id: z.number(),
+});
+
+export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signinInput>;
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
+export type DeleteBlogInput = z.infer<typeof deleteBlogInput>;
+
